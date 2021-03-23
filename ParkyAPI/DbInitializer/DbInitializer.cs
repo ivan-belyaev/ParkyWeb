484a38847fbfe0246bc3b95a128a24ac4c31f20a
Ibelyaev <Ibelyaev@DESKTOP>
2021-03-23 08:31:34 +0500

    Added two parks

--- ParkyAPI/DbInitializer/DbInitializer.cs
+++ ParkyAPI/DbInitializer/DbInitializer.cs
@@ -1,8 +1,10 @@
 ﻿
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using ParkyAPI.Data;
 using ParkyAPI.Models;
 using System;
+using System.IO;
 using System.Linq;
 
 namespace ParkyAPI.DDbInitializer
@@ -10,11 +12,13 @@ namespace ParkyAPI.DDbInitializer
     public class DbInitializer : IDbInitializer
     {
         private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
 
-        public DbInitializer(ApplicationDbContext db)
+        public DbInitializer(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public void Initalize()
@@ -49,6 +53,22 @@ namespace ParkyAPI.DDbInitializer
                 Username = "test",
                 Password = "test",
                 Role = "User"
+            });
+
+            _db.NationalParks.Add(new NationalPark
+            {
+                Name = "Bryce National Park",
+                State = "UT",
+                Established = new DateTime(1981, 1, 1),
+                Picture = File.ReadAllBytes(Path.Combine(_webHostEnvironment.ContentRootPath, "DbInitializer\\img\\BryceNationalPark.jpg"))
+            });
+
+            _db.NationalParks.Add(new NationalPark
+            {
+                Name = "Glacier National Park",
+                State = "YW",
+                Established = new DateTime(1845, 1, 1),
+                Picture = File.ReadAllBytes(Path.Combine(_webHostEnvironment.ContentRootPath, "DbInitializer\\img\\GlacierNationalPark.jpg"))                
             });
 
             _db.SaveChanges();
